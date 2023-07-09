1c2631dec1426b6fcf38ac69e86ad90246d8f0a4
spxerek <138988935+spxerek@users.noreply.github.com>
2023-07-09 11:18:36 +0530

    mac_changer.py

--- mac_changer.py
+++ mac_changer.py
@@ -27,7 +27,7 @@ def change_mac(interface, new_mac):
 
 def get_current_mac(interface):
     ifconfig_result = subprocess.check_output(["ifconfig", interface])
-    mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    mac_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
     if mac_result is not None:
         return mac_result.group(0)
     else:
@@ -37,12 +37,15 @@ def get_current_mac(interface):
 options = get_arguments()
 
 current_mac = get_current_mac(options.interface)
-print("Current MAC = "+str(current_mac))
+if current_mac is not None:
+    print("Current MAC = "+str(current_mac))
 
-change_mac(options.interface, options.new_mac)
+    change_mac(options.interface, options.new_mac)
 
-current_mac = get_current_mac(options.interface)
-if current_mac == options.new_mac:
-    print("[+] MAC address was successfully changed to " + current_mac)
+    current_mac = get_current_mac(options.interface)
+    if current_mac == options.new_mac:
+        print("[+] MAC address was successfully changed to " + current_mac)
+    else:
+        print("[-] MAC did not get changed")
 else:
-    print("[-] MAC did not get changed")
+    print("[-] "+options.interface+" does not have a MAC Address")
